7cbd500e85e1fc7ce6c81976bf3552b04f8e4523
dipojjal <cooldipojjal@gmail.com>
2017-06-02 20:40:02 +0530

    testConnect
    testConnect

--- testConnect.php
+++ testConnect.php
@@ -2,7 +2,7 @@
 # This function reads your DATABASE_URL configuration automatically set by Heroku
 # the return value is a string that will work with pg_connect
 function pg_connection_string() {
-  return "dbname=abcdefg host=****.amazonaws.com port=5432 user=**** password=**** sslmode=require";
+  return "dbname=delibg7376jbnr host=ec2-23-23-234-118.compute-1.amazonaws.com port=5432 user=bmgnjpnaetiyzq password=b69bb793b59467cec59c95f212291d07d48053a91eb9e8fd40d990dee914c369 sslmode=require";
 }
  
 # Establish db connection
@@ -11,6 +11,9 @@ if (!$db) {
     echo "Database connection error."
     exit;
 }
+
+else{
+echo "DB Connection successful";
+}
  
-$result = pg_query($db, "SELECT statement goes here");
 ?>
