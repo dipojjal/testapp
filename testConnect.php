ecfd6771c87dd58ee469aa03c63cffbd2d22d812
dipojjal <cooldipojjal@gmail.com>
2017-06-04 10:20:12 +0530

    reverted back to original file

--- testConnect.php
+++ testConnect.php
@@ -39,32 +39,8 @@ $result->closeCursor();
   echo "Connection failed";
   }
     
-    function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
-# Here we establish the connection. Yes, that's all.
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
-    
-    if ($pg_conn){
-    echo "conn success";
-    }
-    
-    else{
-    echo "unsuccessful";
-    }
-    
-# Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT * FROM account.salesforce");
-print "<pre>\n";
-if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-} else {
-  print "Tables in your database:\n";
-   }
-}
-print "\n";
-  
+
 ?>
 </body>
 </html>
