7bc419e2556d5ee86fbe6e12e96e98506fa891d0
dipojjal <cooldipojjal@gmail.com>
2017-06-05 20:29:39 +0530

    username

--- productFirstPage.php
+++ productFirstPage.php
@@ -51,9 +51,9 @@ function pg_connection_string_from_database_url() {
 
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
-$result = pg_query($pg_conn, "SELECT username, password FROM salesforce.Heroku_User__c where username=$user && password=$pwd");
+$result = pg_query($pg_conn, "SELECT username, password FROM salesforce.Heroku_User__c where username=$user");
 print "<pre>\n";
-if (!pg_num_rows($result)) {
+if (!$result) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
 } else {
   print "Records in the Accounts:\n";
