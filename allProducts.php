41a6c5791cc8bbc0598bc6781f5ccc6d0dd95fc6
dipojjal <cooldipojjal@gmail.com>
2017-06-05 22:35:30 +0530

    products

---
+++ allProducts.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<?php
+
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+
+$result = pg_query($pg_conn, "SELECT Name FROM salesforce.Account");
+print "<pre>\n";
+if (!pg_num_rows($result)) {
+  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+} else {
+  print "Records in the Accounts:\n";
+  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+}
+print "\n";
+?>
+</body>
+</html>
