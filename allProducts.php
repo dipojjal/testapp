5db5a03fa9030d9c2ede9bd940bf738455cebda5
dipojjal <cooldipojjal@gmail.com>
2017-06-06 00:32:19 +0530

    allProducts

--- allProducts.php
+++ allProducts.php
@@ -17,7 +17,7 @@ $prodName=$_POST["searchText"];
 $result = pg_query($pg_conn, "SELECT Id, Name, sfid, isactive, country__c, state__c FROM salesforce.Product2 Where Name= $prodName");
 print "<pre>\n";
 if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+  print("No products match your query.\n");
 } else {
   print "Records in the Accounts:\n";
   while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
