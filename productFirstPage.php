d9f6402ab9ec6b99edbd07606119e8e282dc1c8a
dipojjal <cooldipojjal@gmail.com>
2017-06-05 20:13:03 +0530

    login checking

--- productFirstPage.php
+++ productFirstPage.php
@@ -37,6 +37,30 @@
 </style>
 </head>
 <body style="background-color: white;">
+<?php
+
+$user = $_POST("uname");
+$pwd= $_POST("pswd");
+
+# This function reads your DATABASE_URL config var and returns a connection
+# string suitable for pg_connect. Put this in your app.
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+
+$result = pg_query($pg_conn, "SELECT username, password FROM salesforce.Heroku_User__c where username=$user && password=$pwd");
+print "<pre>\n";
+if (!pg_num_rows($result)) {
+  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+} else {
+  print "Records in the Accounts:\n";
+  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+}
+print "\n";
+
 <div id="d"><center>
 <h1>Product Management</h1></center></div>
 <div id="d1"><center>
@@ -45,5 +69,8 @@
 <button class="button">Update Product</button>
 <button class="button">Delete Product</button></center>
 </div>
+
+?>
+
 <body>
 </html>
