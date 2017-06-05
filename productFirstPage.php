dcdc98c6ccf23cfe6e9251ccd6d1312ce6d6e456
dipojjal <cooldipojjal@gmail.com>
2017-06-05 12:18:07 +0530

    productFirstPage

---
+++ productFirstPage.php
@@ -0,0 +1,49 @@
+<head>
+<style>
+.button {
+  display: inline-block;
+  padding: 75px 105px;
+  font-size: 24px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  outline: none;
+  color: #fff;
+  background-color: #4CAF50;
+  border: none;
+  border-radius: 15px;
+  box-shadow: 0 9px #999;
+}
+
+#d {
+    border-radius: 5px;
+    background-color: #5CE4F3;
+    padding: 25px;
+	
+}
+#d1 {
+    border-radius: 5px;
+ 
+    padding: 25px;
+}
+
+.button:hover {background-color: #3e8e41}
+
+.button:active {
+  background-color: #5CE4F3;
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
+}
+</style>
+</head>
+<body style="background-color: white;">
+<div id="d"><center>
+<h1>Product Management</h1></center></div>
+<div id="d1"><center>
+<button class="button">Search Product</button>
+<button class="button">Create Product</button></br></br>
+<button class="button">Update Product</button>
+<button class="button">Delete Product</button></center>
+</div>
+<body>
+</html>
