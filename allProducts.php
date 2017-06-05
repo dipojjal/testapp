abcd22bf274e55df3046c566ef5e8b6a91a901c2
dipojjal <cooldipojjal@gmail.com>
2017-06-06 01:59:53 +0530

    prodName

--- allProducts.php
+++ allProducts.php
@@ -15,13 +15,13 @@ $pg_conn = pg_connect(pg_connection_string_from_database_url());
 $prodName=$_POST["searchText"];
 echo $prodName;
 
-$result = pg_query($pg_conn, "SELECT name, sfid, isactive, country__c, state__c FROM salesforce.Product2 where name=".$prodName.";");
+$result = pg_query($pg_conn, "SELECT Name, sfid, isactive, country__c, state__c FROM salesforce.Product2 where Name=prodName;");
 print "<pre>\n";
 
   print "Records in the Products:\n";
   while ($row = pg_fetch_row($result)) { print("- $row[0] - $row[1] - $row[2]\n"); }
 
-print "\n";
+print "\n"; 
 
 ?>
 </body>
