eb40d5de9dcb4bc1f88880ec22bbf9319e892f0b
dipojjal <cooldipojjal@gmail.com>
2017-06-06 12:15:40 +0530

    login and logout pages

---
+++ signup.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<link rel = "stylesheet" type = "text/css" href = "myfile.css" />
+</head>
+<body>
+<!-- multistep form -->
+<form id="msform">
+   
+  <fieldset>
+    <h2 class="fs-title">Create your account</h2>
+    <input type="text" name="email" placeholder=" First Name" />
+    <input type="text" name="email" placeholder="Last Name" /> <br/> <br/>
+    <input type="radio" name="gender" value="male" checked> Male </input> &nbsp; &nbsp; &nbsp;
+    <input type="radio" name="gender" value="female"> Female </input> <br/> <br/> 
+    <input type="text" name="email" placeholder="Email" />
+    <input type="password" name="pass" placeholder="Password" />
+    <input type="password" name="cpass" placeholder="Confirm Password" /> <br/>
+    <input type="button" name="Submit" class="next action-button" value="Submit" />
+  </fieldset>
+</form>
+</body>
+</html>
