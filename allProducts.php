85152006ab5b08867d0473b2b073cb891ad64192
dipojjal <cooldipojjal@gmail.com>
2017-06-08 18:48:32 +0530

    border

--- allProducts.php
+++ allProducts.php
@@ -20,7 +20,7 @@ if (!$result) {
   exit;
 }
   echo "<div class=\"table-responsive\">
-  <table class=\"table-hover\"> border=3px";
+  <table class=\"table-hover\"> border=3";
   echo "<tr>";
   echo "<th>ID</th>";
   echo "<th>NAME</th>";
