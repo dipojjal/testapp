1e3d58f3ee90bb373948d45f6b205eb4285e81e3
dipojjal <cooldipojjal@gmail.com>
2017-06-12 13:35:18 +0530

    Totally reverted

--- allProducts.php
+++ allProducts.php
@@ -1,48 +1,37 @@
 <html>
 <head>
-<title> All Products</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />  
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">  
+<title></title>
 </head>
 <body>
-  
 <?php
-  
 function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV["DATABASE_URL"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1);
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
-
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
-
  $result = pg_query($pg_conn, "SELECT sfid, Name, isactive, country__c, state__c FROM salesforce.Product2");
 if (!$result) {
   echo "An error occurred.\n";
   exit;
 }
-  echo "<center><h2>All Products List</h2></center>";
-  echo "<table id= "myTable" class=\"table table-striped\">";
-  echo "<thead>";
+  echo "<div class=\"container\"> <table class=\"table table-striped\">";
   echo "<tr>";
-  echo"<th>ID</th>";
+  echo "<th>ID</th>";
   echo "<th>NAME</th>";
   echo "<th>ACTIVE</th>";
   echo "<th>COUNTRY</th>";
   echo"<th>STATE</th>";
   echo "</tr>";
-  echo "</thead>";
-  echo "<tbody id=\"prodTable\">";
-  while ($row = pg_fetch_row($result)) {
+  
+while ($row = pg_fetch_row($result)) {
   echo "<tr>";
   echo "<td>$row[0]</td>". "<td>$row[1]</td>". "<td>$row[2]</td>". "<td>$row[3]</td>". "<td>$row[4]</td>";
   echo "</tr>";
 }
-  echo"</tbody>";
   echo "</table>";
-  
+  echo "</div>";
 ?>
-
-
-  
 </body>
-  
 </html>
