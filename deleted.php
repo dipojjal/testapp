4bb8d9bd93509f2d8cb5d0094302fc33ad5d655f
dipojjal <cooldipojjal@gmail.com>
2017-06-14 09:46:50 +0530

    deleted

--- deleted.php
+++ deleted.php
@@ -8,25 +8,12 @@ function pg_connection_string_from_database_url() {
 $toBeDeleted= $_POST['search'];
 
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
-$toDelete = pg_query($pg_conn, "SELECT sfid, Name, isactive, country__c, state__c FROM salesforce.Product2 where sfid= $toBeDeleted");
+$toDelete = pg_query($pg_conn, "DELETE FROM salesforce.Product2 where sfid= '$toBeDeleted'");
 
-$res = pg_delete($pg_conn, 'post_log', $toDelete);
+if(!$toDelete){
 
-if($toDelete){
-
-echo "Record found!";
-  
-  while ($row = pg_fetch_row($toDelete)) {
-  echo "<tr>";
-  echo "<td>$row[0]</td>". "<td>$row[1]</td>". "<td>$row[2]</td>". "<td>$row[3]</td>". "<td>$row[4]</td>";
-  echo "</tr>";
-}
-  
-}
-
-if (!$res) {
-  echo "Record not found";
-  exit;
+echo "Record not found!";
+ 
 }
 
 else{
