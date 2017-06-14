35e5d1447e8cf5e487e6ac1d27998c97a35ef25d
dipojjal <cooldipojjal@gmail.com>
2017-06-14 13:25:45 +0530

    like operator

--- searchedProds.php
+++ searchedProds.php
@@ -15,8 +15,10 @@ function pg_connection_string_from_database_url() {
 
 $varSearchedItem= $_POST['search'];
 
+  $searchTerm= '%'.$varSearchedItem.'%';
+  
   $pg_conn = pg_connect(pg_connection_string_from_database_url());
-  $searchedProds = pg_query($pg_conn, "SELECT Id, sfid, Name, productcode, isactive, country__c, state__c, description FROM salesforce.Product2 where Name= '$varSearchedItem' || Id= '$varSearchedItem' || sfid= '$varSearchedItem' || productcode= '$varSearchedItem'");
+  $searchedProds = pg_query($pg_conn, "SELECT Id, sfid, Name, productcode, isactive, country__c, state__c, description FROM salesforce.Product2 where Name like '$searchTerm' OR Id like '$searchTerm' OR sfid like '$searchTerm' OR productcode like '$searchTerm'");
 
   echo"<div class=\"panel panel-default\">";
   
