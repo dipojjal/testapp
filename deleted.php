36145245cef79020f0d68f26b55c402aea9d9c83
dipojjal <cooldipojjal@gmail.com>
2017-06-14 08:48:54 +0530

    deletion code

--- deleted.php
+++ deleted.php
@@ -1,9 +1,22 @@
 <?php
+  
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
 
+$toBeDeleted= $_POST['search'];
 
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+$toDelete = pg_query($pg_conn, "SELECT sfid, Name, isactive, country__c, state__c FROM salesforce.Product2 where sfid=". $toBeDeleted);
 
-echo "the record has been deleted";
+if (!$toDelete) {
+  echo "Record not found";
+  exit;
+}
 
-echo "Record not found";
+else{
+  echo "the record has been deleted";
+}
 
 ?>
