548f8fa3b94e88cb0224ca9e028234d14988634c
dipojjal <cooldipojjal@gmail.com>
2017-06-14 11:16:33 +0530

    added columns

--- allProducts.php
+++ allProducts.php
@@ -11,7 +11,7 @@ function pg_connection_string_from_database_url() {
   return "user=$user password=$pass host=$host dbname=" . substr($path, 1);
 }
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
- $result = pg_query($pg_conn, "SELECT sfid, Name, isactive, country__c, state__c FROM salesforce.Product2");
+ $result = pg_query($pg_conn, "SELECT Id, sfid, Name, isactive, productcode, description, country__c, state__c FROM salesforce.Product2");
 if (!$result) {
   echo "An error occurred.\n";
   exit;
@@ -23,16 +23,19 @@ if (!$result) {
   echo"<div class=\"panel-body\">";
   echo "<div class=\"container\"> <table class=\"table table-striped\">";
   echo "<tr>";
-  echo "<th>ID</th>";
+  echo "<th>ID</th>"
+  echo "<th>SALESFORCE ID</th>";
   echo "<th>NAME</th>";
   echo "<th>ACTIVE</th>";
+  echo "<th>PRODUCT CODE</th>";
+  echo "<th>DESCRIPTION</th>";
   echo "<th>COUNTRY</th>";
   echo"<th>STATE</th>";
   echo "</tr>";
   
 while ($row = pg_fetch_row($result)) {
   echo "<tr>";
-  echo "<td>$row[0]</td>". "<td>$row[1]</td>". "<td>$row[2]</td>". "<td>$row[3]</td>". "<td>$row[4]</td>";
+  echo "<td>$row[0]</td>". "<td>$row[1]</td>". "<td>$row[2]</td>". "<td>$row[3]</td>". "<td>$row[4]</td>". "<td>$row[5]</td>". "<td>$row[6]</td>". "<td>$row[7]</td>";
   echo "</tr>";
 }
   echo "</table>";
