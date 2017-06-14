947949c549e05376185fd60350e3cab98ae247cb
dipojjal <cooldipojjal@gmail.com>
2017-06-14 08:52:05 +0530

    created the code

--- deleted.php
+++ deleted.php
@@ -10,7 +10,9 @@ $toBeDeleted= $_POST['search'];
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 $toDelete = pg_query($pg_conn, "SELECT sfid, Name, isactive, country__c, state__c FROM salesforce.Product2 where sfid=". $toBeDeleted);
 
-if (!$toDelete) {
+$res = pg_delete($pg_conn, 'post_log', $toDelete);
+
+if (!$res) {
   echo "Record not found";
   exit;
 }
