885340684c0c8ae6d96074a6cdb7649f34164122
dipojjal <cooldipojjal@gmail.com>
2017-06-14 12:29:08 +0530

    removed php tags

--- createProduct.php
+++ createProduct.php
@@ -53,14 +53,13 @@ legend {color:red;}
 
 <body>
 
-<?php
-
-$username = $_POST['uname'];
-$password = $_POST['pwd'];
-
-
-
-?>
+    <html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">  
+<title></title>
+</head>
+<body>
 
 <div>
 <form>
